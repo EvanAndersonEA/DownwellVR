f887d8e9a2aa2d23dd77bba436ee1f6b991c26fe
EvanAndersonEA <62121384+EvanAndersonEA@users.noreply.github.com>
2020-03-13 11:32:25 -0700

    Added  big measurer for speed
    added some platforms you can walk off and a measuring tape

--- Assets/Player/Scripts/ShootJump.cs
+++ Assets/Player/Scripts/ShootJump.cs
@@ -44,7 +44,7 @@ public class ShootJump : MonoBehaviour
 
     public void SpawnBullet(GameObject boolet, GameObject player)
     {
-        freshBullet = Instantiate(boolet, player.transform.position - new Vector3(0, 0.5f, 0), Quaternion.identity);
+        freshBullet = Instantiate(boolet, player.transform.position - new Vector3(0, 1f, 0), Quaternion.identity);
         gameObject.GetComponent<Rigidbody>().velocity = new Vector3 (0, 5, 0);
         freshBullet.GetComponent<Rigidbody>().velocity = new Vector3 (0, -25, 0);
     }
