c6b2f983adb7ac6c6cadb7233e226c3ecb0b5c0a
Diego Kaleb Valenzuela Carrillo <d.kal3b@gmail.com>
2020-04-02 10:15:25 -0700

    Creating Breakable Floor
    Description:
    If you shoot over a breakable for n times, it will dissappear.
    
    Create:
    - Bullet Tag.
    - Breakable floor prefab.
    - Breakable floor tag.
    
    Changes:
    - Bullet prefab isTrigger: ON

---
+++ Assets/Player/Scripts/Breakable_Floor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakable_Floor : MonoBehaviour
+{
+    [SerializeField] private int numberOfHitsBeforeDestroyed = 3;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Bullet"))
+        {
+            numberOfHitsBeforeDestroyed--;
+            if (numberOfHitsBeforeDestroyed == 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
