473135e1a58f733bb8361c7935029aebb1c70c19
Diego Kaleb Valenzuela Carrillo <d.kal3b@gmail.com>
2020-04-13 20:31:11 -0700

    Pull

---
+++ Assets/Player/Scripts/Start_Game.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Start_Game : MonoBehaviour
+{
+    [SerializeField] private GameObject start_Menu;
+    [SerializeField] private AudioSource mainSong;
+    [SerializeField] private GameObject start_Platform;
+
+    public Start_Game()
+    {
+        start_Menu.SetActive(false);
+        start_Platform.SetActive(false);
+        //mainSong 
+    }
+}
