a0b079a8ab65dd4ca5b1bbbedabc5e3ef8649b0c
Diego Kaleb Valenzuela Carrillo <d.kal3b@gmail.com>
2020-04-14 09:06:40 -0700

    Update Start

--- Assets/Player/Scripts/Start_Game.cs
+++ Assets/Player/Scripts/Start_Game.cs
@@ -1,17 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 
 public class Start_Game : MonoBehaviour
 {
-    [SerializeField] private GameObject start_Menu;
-    [SerializeField] private AudioSource mainSong;
-    [SerializeField] private GameObject start_Platform;
+    [SerializeField] private SteamVR_ActionSet m_ActionSet = null;
+    [SerializeField] private SteamVR_Action_Boolean m_BooleanAction = null;
+    [SerializeField] private GameObject start_Menu = null;
+    [SerializeField] private AudioSource mainSong = null;
+    [SerializeField] private GameObject start_Platform = null;
+    private bool hasTheGameStarted = false;
 
-    public Start_Game()
+    private void Awake()
+    {
+        m_ActionSet.Activate(SteamVR_Input_Sources.Any, 0, true);
+    }
+
+    private void Update()
+    {
+        if (!hasTheGameStarted)
+        {
+            if (m_BooleanAction.GetStateDown(SteamVR_Input_Sources.Any))
+            {
+                StartGame();
+                hasTheGameStarted = false;
+
+            }
+        }
+    }
+
+    private void StartGame()
     {
         start_Menu.SetActive(false);
         start_Platform.SetActive(false);
-        //mainSong 
+        mainSong.Play();
     }
 }
